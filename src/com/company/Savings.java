3916249c95fce102b74ec1bf0fda2db451faaf64
Lukasz Ratajczak <l.m.ratajczak94@gmail.com>
2019-03-22 15:43:39 +0100

    added types of account classes

---
+++ src/com/company/Savings.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Savings {
+    Double availableFunds;
+    String opepingDate;
+    Integer IBAN;
+    String ownerFullName;
+
+
+
+
+}
---
+++ src/com/company/Standard.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Standard {
+    Double availableFunds;
+    String opepingDate;
+    Integer IBAN;
+    String ownerFullName;
+
+}
