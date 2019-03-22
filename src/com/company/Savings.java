c36830a6ec2e64f3e42924f4ef729839bc0f3f7e
Lukasz Ratajczak <l.m.ratajczak94@gmail.com>
2019-03-22 15:56:32 +0100

    added gets,sets, constructors and private/priv-final to accounts

--- src/com/company/Savings.java
+++ src/com/company/Savings.java
@@ -1,11 +1,43 @@
 package com.company;
 
 public class Savings {
-    Double availableFunds;
-    String opepingDate;
-    Integer IBAN;
-    String ownerFullName;
+    private Double availableFunds;
+    private final String opepingDate;
+    private final Integer IBAN;
+    private String ownerFullName;
 
+
+    public Savings(Double availableFunds, String opepingDate, Integer IBAN, String ownerFullName) {
+        this.availableFunds = availableFunds;
+        this.opepingDate = opepingDate;
+        this.IBAN = IBAN;
+        this.ownerFullName = ownerFullName;
+    }
+
+
+    public void setOwnerFullName(String ownerFullName) {
+        this.ownerFullName = ownerFullName;
+    }
+
+    public Double getAvailableFunds() {
+        return availableFunds;
+    }
+
+    public void setAvailableFunds(Double availableFunds) {
+        this.availableFunds = availableFunds;
+    }
+
+    public String getOpepingDate() {
+        return opepingDate;
+    }
+
+    public Integer getIBAN() {
+        return IBAN;
+    }
+
+    public String getOwnerFullName() {
+        return ownerFullName;
+    }
 
 
 
